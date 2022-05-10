e78d56d0c3d9106ced177f6d730a5be583c0f06c
Soulivanh Thao <sthao@lsce.ipsl.fr>
2022-05-10 23:12:42 +0200

    postBuild

--- install.R
+++ install.R
@@ -1,2 +1,2 @@
 install.packages(c("methods", "R6", "Lmoments", "numDeriv", "Rcpp", "RcppEigen", "RColorBrewer"))
-install.packages(c("ROOPSD_0.3.6.tar.gz", "SBCK_0.5.0.tar.gz"), type = "source"))
+# install.packages(c("ROOPSD_0.3.6.tar.gz", "SBCK_0.5.0.tar.gz"), type = "source"))
